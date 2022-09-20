07c6d6ff5913eb78c57a65bdd46e1c7804544537
lokesh <devathilokesh2001@gmail.com>
2022-09-20 14:09:20 +0530

    commit

--- src/app/categories/categories.component.ts
+++ src/app/categories/categories.component.ts
@@ -25,19 +25,19 @@ export class CategoriesComponent implements OnInit {
     this.afs.collection('categories').add(categoryData).then(docRef=>{
       console.log(docRef);
 
-      this.afs.doc(`categories/${docRef.id}`).collection('subcategories').add(subCategoryData)
+      // this.afs.doc(`categories/${docRef.id}`).collection('subcategories').add(subCategoryData)
 
        this.afs.collection('categories').doc(docRef.id).collection('subcategories').add(subCategoryData).then(docRef1 => {
           console.log(docRef1);
           
 
-          this.afs.doc(`categories/${docRef.id}/subcategories/${docRef1.id}`).collection('subsubcategories').add(subCategoryData)
+          // this.afs.doc(`categories/${docRef.id}/subcategories/${docRef1.id}`).collection('subsubcategories').add(subCategoryData)
 
            
            
           this.afs.collection('categories').doc(docRef.id).collection('subcategories').doc(docRef1.id).collection('subsubcategories').add(subCategoryData).then(docRef2 => {
             console.log('Second Level Subcategory Saved Successfully');
-          })
+          });
       })
     })
     .catch(err=>{console.log(err)})
