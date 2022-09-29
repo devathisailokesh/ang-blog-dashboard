43a4e01fd7e930c86a182334867628d91c6256d8
lokesh <devathilokesh2001@gmail.com>
2022-09-29 22:16:14 +0530

    commit

--- src/app/posts/all-post/all-post.component.ts
+++ src/app/posts/all-post/all-post.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PostsService } from 'src/app/services/posts.service';
 
 @Component({
   selector: 'app-all-post',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AllPostComponent implements OnInit {
 
-  constructor() { }
+  constructor(private postservice: PostsService) { }
 
   ngOnInit(): void {
+
+    this.postservice.loadData().subscribe(val => {
+      console.log(val);
+    })
   }
 
 }
