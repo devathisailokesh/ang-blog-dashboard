a124605979274f97eaa9fe35a058766052f9c844
lokesh <devathilokesh2001@gmail.com>
2022-10-02 10:20:29 +0530

    commit

--- src/app/services/subscribers.service.ts
+++ src/app/services/subscribers.service.ts
@@ -1,14 +1,14 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
-import { ToastrService } from 'ngx-toastr'
-import { map } from 'rxjs';
+import { ToastrService } from 'ngx-toastr';
+import { map } from 'rxjs/operators';
 @Injectable({
   providedIn: 'root'
 })
 export class SubscribersService {
-  toastr: any;
+  toastr: any;  
 
-  constructor( private afs: AngularFirestore ) { }
+  constructor( private afs: AngularFirestore, private Toastr: ToastrService ) { }
 
   loadData(){
 
